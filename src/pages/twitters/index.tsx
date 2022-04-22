162cafb94393f93358e846440296033152d65fb4
Rafael Chicarelli <rafaelsep20@gmail.com>
2022-04-21 22:30:03 -0300

    refactor: Passing the data fetching to ssr

--- src/pages/twitters/index.tsx
+++ src/pages/twitters/index.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-
 import Tweet from "../../components/Tweet";
 import ApiService, { IPost, IMeta } from "../../services/utils/ApiService";
 
@@ -8,20 +7,27 @@ import {
   ListContainer
 } from './styles';
 
-const Twitters = () => {
-  const [tweets, setTweets] = useState<IPost[]>([]);
-  const [meta, setMeta] = useState({} as IMeta);
+//ssr
+import TwitterService from '../../services/utils/TwitterService';
 
-  useEffect(() => {
-    ApiService.listTweets()
-    .then(response => {
-      setTweets(response.posts);
-      setMeta(response.meta);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-  }, []);
+interface TwittersProps {
+  posts: IPost[],
+  meta: IMeta
+};
+const Twitters = (props: TwittersProps) => {
+  const [tweets, setTweets] = useState<IPost[]>(props.posts);
+  const [meta, setMeta] = useState<IMeta>(props.meta);
+
+  // useEffect(() => {
+  //   ApiService.listTweets()
+  //   .then(response => {
+  //     setTweets(response.posts);
+  //     setMeta(response.meta);
+  //   })
+  //   .catch(error => {
+  //     console.log(error);
+  //   });
+  // }, []);
 
   return (
     <Container>
@@ -42,3 +48,42 @@ const Twitters = () => {
 }
 
 export default Twitters;
+
+export async function getServerSideProps() {
+
+  const allData = await TwitterService.listTweets();
+
+  const posts = allData.data;
+  const users = allData.includes.users;
+
+  const postFromUsersPalmeirenses = posts.filter(post => {
+    const foundUser = users.find((user) => user.id === post.author_id);
+    if (!foundUser) return false;
+
+    let isFan = false;
+    let lowerDescription = foundUser.description.toLowerCase();
+
+    if (foundUser.name.indexOf('sep') !== -1) isFan = true;
+    if (lowerDescription.includes('palmeir')) isFan = true;
+    if (lowerDescription.includes('verdão')) isFan = true;
+    if (lowerDescription.includes('mancha verde')) isFan = true;
+
+    return isFan;
+  });
+
+  const latestPostsFromFans = postFromUsersPalmeirenses.map(post => {
+    const foundUser = users.find((user) => user.id === post.author_id);
+
+    return ({
+      ...post,
+      user: foundUser,
+    });
+  });
+
+  return {
+    props: {
+      posts: latestPostsFromFans,
+      meta: allData.meta
+    }
+  }
+}
